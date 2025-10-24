66d04d19f144ba18cfef868e6d51f822b781ffda
Abrahim MeJia Valdez <abrmeval@outlook.com>
2025-10-23 21:05:07 -0600

    Initialize first app with basic file system operations and output file

---
+++ first-app/first-app.js
@@ -0,0 +1,6 @@
+//import the filesystem module
+import fs from 'fs';
+
+// Write "Hello, World!" to output.txt
+fs.writeFileSync('output.txt', 'Hello, World!');
+console.log("This is the first app file.");
